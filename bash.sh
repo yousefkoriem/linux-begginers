68e76ed2ad8d0ca23f0dea03e59f2c183ba200d8
yousefkoriem <koriemyousef@gmail.com>
2022-07-17 20:24:11 +0200

    new edit

--- bash.sh
+++ bash.sh
@@ -16,31 +16,38 @@ sleep 5
 clear
 
 else
-# cp -r .bash_aliases ~/
-cp -r .alias/ ~/
+mv .bash_aliases ~/
+mv .alias/ ~/
 clear
 printf "You can run update and upgrade by typing <update>\n"
 sleep 1
-printf "Want to try? \033[93m\"type update\" "
+printf "Want to try? \033[93m\"type update\" \033[0m"
 read b
 if [ $b == 'update' ]
 then
 bash ~/.alias/update.sh
-else
-printf "Ok next thing\n"
-sleep 2
-
 fi
+printf "Ok, next thing\n"
+sleep 2
 clear
 printf "Next thing\n"
 printf "You can run fix if you have problem in packages by typing <fix>\n"
 sleep 1
-printf "Want to try? \033[93m\"type fix\" "
+printf "Want to try? \033[93m\"type fix\" \033[0m"
 read b
 if [ $b == 'fix' ]
 then
 bash ~/.alias/fix.sh
-else
-printf "OK goodbye for now\n"
 fi
 fi
+printf "Ok, let's see the real questions"
+sleep 3
+clear
+printf "That's all\n"
+sleep 3
+printf "\033[92mYour Terminal will close to apply changes \033[0m"
+sleep 5
+clear
+printf "\033[92mGood bye, See you soon \033[0m\n"
+sleep 4
+exit
